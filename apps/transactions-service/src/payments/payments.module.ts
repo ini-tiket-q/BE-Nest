06b8ec8ae936727c93f600fd7429d7c62340f9d7
Salman <salmanalthof31@gmail.com>
2026-01-06 13:21:10 +0700

    feat: import paymentModule infra

--- apps/transactions-service/src/payments/payments.module.ts
+++ apps/transactions-service/src/payments/payments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { MidtransSnapAdapter, PaymentModule } from '@tiketq-be/payment';
 
 @Module({
+  imports: [PaymentModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
