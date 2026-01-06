71c580472fe5de98ac301b007a405182007c9dd9
Salman <salmanalthof31@gmail.com>
2026-01-06 13:24:33 +0700

    refactor: change output createTransaction and use IMidtransPaymentPort

--- apps/transactions-service/src/payments/payments.service.ts
+++ apps/transactions-service/src/payments/payments.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/req/create-payment.dto';
+import { IMidtransPaymentPort } from 'libs/domain/src/transaction/ports';
 
 @Injectable()
 export class PaymentsService {
-
-  async createTransaction(order: CreatePaymentDto): Promise<string | number> {
-    return order.order_id + 'nice';
+  constructor(@Inject('IMidtransPaymentPort') private readonly midtransSnapAdapter: IMidtransPaymentPort) {}
+  async createTransaction(order: CreatePaymentDto): Promise<{ token: string, redirect_url: string }> {
+    const param = {
+      transaction_details: {
+        order_id: order.order_id,
+        gross_amount: order.gross_amount
+      }
+    }
+    const generate = await this.midtransSnapAdapter.generateSnapUrl(param)
+    return generate
   }
 
 }
