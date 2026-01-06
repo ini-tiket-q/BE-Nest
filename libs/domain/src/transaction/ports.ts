e4d41f132bc9e6c07e031d14c093aa545bc9e84c
Salman <salmanalthof31@gmail.com>
2026-01-06 13:18:10 +0700

    feat: change ouput generateSnapUrl

--- libs/domain/src/transaction/ports.ts
+++ libs/domain/src/transaction/ports.ts
@@ -4,5 +4,5 @@ export interface IMidtransPaymentPort {
   /**
    * Generates the redirect URL for the user to pay
    */
-  generateSnapUrl(params: PaymentParams): Promise<string>;
+  generateSnapUrl(params: PaymentParams): Promise<{ token: string, redirect_url: string }>;
 }
