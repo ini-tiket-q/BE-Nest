d2d3518f3f9c5ec6a148334f41d258614261ab9e
Salman <salmanalthof31@gmail.com>
2026-01-06 22:28:28 +0700

    feat: create custom error for use type data error in midtrans-error.mapper.ts

---
+++ libs/infrastructure/payment/src/exception/midtrans-http-error.ts
@@ -0,0 +1,6 @@
+export class MidtransHttpError extends Error {
+    constructor(public readonly status: number, message?: string) {
+        super(message);
+        this.name = 'MidtransHttpError'
+    }
+}
