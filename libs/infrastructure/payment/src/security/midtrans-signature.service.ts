389d31b774783627f77cf90f55a57822ea4a13d8
Lily <lilywinslettt@gmail.com>
2026-01-09 00:35:07 +0800

    feat: add logic for signature service

--- libs/infrastructure/payment/src/security/midtrans-signature.service.ts
+++ libs/infrastructure/payment/src/security/midtrans-signature.service.ts
@@ -1,3 +1,19 @@
-export function verifySignature(): boolean {
-    return true;
+import { createHash } from "crypto";
+
+export function verifySignature(
+  order_id: string,
+  status_code: string,
+  gross_amount: string,
+  signature_key: string
+): boolean {
+  const server_key = process.env['SERVER_KEY'] || '';
+
+  const input = order_id + status_code + gross_amount + server_key;
+
+  const encrypted_input = createHash('sha512').update(input).digest('hex');
+
+  if (encrypted_input !== signature_key) {
+    return false;
+  }
+  return true;
 }
