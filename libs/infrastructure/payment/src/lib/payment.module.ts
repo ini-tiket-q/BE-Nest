85864efe43c4b4bd79bee6368b706b46a31c059e
Salman <salmanalthof31@gmail.com>
2026-01-09 22:35:21 +0700

    refactor: delete module MidtransClient because logic move to midtrans-snap.adapter.ts

--- libs/infrastructure/payment/src/lib/payment.module.ts
+++ libs/infrastructure/payment/src/lib/payment.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { MidtransSnapAdapter } from '../adapters/midtrans-snap.adapter';
-import { MidtransClient } from './transactions/clients/midtrans.client';
 
 @Module({
   imports: [HttpModule],
   controllers: [],
-  providers: [{ provide: 'IMidtransPaymentPort', useClass: MidtransSnapAdapter }, MidtransClient],
+  providers: [{ provide: 'IMidtransPaymentPort', useClass: MidtransSnapAdapter }],
   exports: ['IMidtransPaymentPort'],
 })
 export class PaymentModule {}
