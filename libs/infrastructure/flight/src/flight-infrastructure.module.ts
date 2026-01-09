d4fe53c780f6affabd66db0f22e1c7b1b66bf325
enrico-zd <enricozada00@gmail.com>
2026-01-09 18:53:39 +0700

    chore(infrastructure): register MMBC HTTP module and interceptor

--- libs/infrastructure/flight/src/flight-infrastructure.module.ts
+++ libs/infrastructure/flight/src/flight-infrastructure.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
-import {IssueTicketProcessor} from './processors/issue-ticket.processor';
+import { IssueTicketProcessor } from './processors/issue-ticket.processor';
 import { FlightInternalController } from './controllers/flight-internal.controller';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { mmbcHttpConfig } from './config/mmbc-http.config';
+import { MmbcAuthInterceptor } from './interceptors/mmbc-auth.interceptor';
 
 @Module({
-    imports: [
-        BullModule.forRoot({
-            connection: {
-                host: 'localhost',
-                port: 6379,
-            },
-        }),
-        BullModule.registerQueue({
-            name: 'flight-issuance',
-        }),
-    ],
-    providers: [IssueTicketProcessor],
-    controllers: [FlightInternalController],
+  imports: [
+    ConfigModule,
+    HttpModule.registerAsync(mmbcHttpConfig),
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'flight-issuance',
+    }),
+  ],
+  providers: [MmbcAuthInterceptor, IssueTicketProcessor],
+  controllers: [FlightInternalController],
 })
 export class FlightInfrastructureModule {}
