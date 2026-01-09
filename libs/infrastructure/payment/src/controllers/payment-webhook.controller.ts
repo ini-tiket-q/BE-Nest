5a4aa6bea21d794d1b60f8faaa463cedbe66e21a
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2026-01-09 16:55:49 +0700

    feat: use verify signature in controller

--- libs/infrastructure/payment/src/controllers/payment-webhook.controller.ts
+++ libs/infrastructure/payment/src/controllers/payment-webhook.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { MidtransCallbackDto } from './payment-webhook.dto';
+import { verifySignature } from '../security/midtrans-signature.service';
 
 @Controller('payments')
 export class PaymentWebhookController {
@@ -7,5 +8,18 @@ export class PaymentWebhookController {
   @HttpCode(HttpStatus.OK)
   midtransTransaction(@Body() dto: MidtransCallbackDto) {
     console.log(`payload:: ${JSON.stringify(dto)}`);
+
+    const isVerified = verifySignature(
+      dto.order_id,
+      dto.status_code,
+      dto.gross_amount,
+      dto.signature_key
+    );
+
+    if (isVerified) {
+      console.log('success verify signature');
+    } else {
+      console.log('failed to verify signature');
+    }
   }
 }
