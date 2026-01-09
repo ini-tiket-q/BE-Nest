72f817a3c5dfca01f05ee54ce7a31848d9491253
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2026-01-09 16:51:47 +0700

    feat: add dto structure and log dto

--- libs/infrastructure/payment/src/controllers/payment-webhook.controller.ts
+++ libs/infrastructure/payment/src/controllers/payment-webhook.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { MidtransCallbackDto } from './payment-webhook.dto';
 
 @Controller('payments')
 export class PaymentWebhookController {
   @Post('callbacks/midtrans')
   @HttpCode(HttpStatus.OK)
-  midtransTransaction() {}
+  midtransTransaction(@Body() dto: MidtransCallbackDto) {
+    console.log(`payload:: ${JSON.stringify(dto)}`);
+  }
 }
---
+++ libs/infrastructure/payment/src/controllers/payment-webhook.dto.ts
@@ -0,0 +1,9 @@
+export class MidtransCallbackDto {
+  signature_key!: string;
+
+  order_id!: string;
+
+  status_code!: string;
+
+  gross_amount!: string;
+}
