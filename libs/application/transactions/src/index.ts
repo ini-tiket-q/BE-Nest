f3644ebde38c2c3db567274ef670aa18d592ba7f
rijal-m-kamil <rijalmkamil99@gmail.com>
2026-01-23 09:51:00 +0700

    feat: add getTransactionDetail endpoint

--- libs/application/transactions/src/index.ts
+++ libs/application/transactions/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/transactions.module';
 export * from './lib/use-cases/create-transaction.use-case';
 export * from './lib/use-cases/get-transactions.interface';
 export * from './lib/use-cases/get-transactions.use-case';
+export * from './lib/use-cases/get-transaction-detail.use-case';
--- libs/application/transactions/src/lib/transactions.module.ts
+++ libs/application/transactions/src/lib/transactions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@tiketq-be/database';
 import { CreateTransactionUseCase } from './use-cases/create-transaction.use-case';
+import { GetTransactionDetailUseCase } from './use-cases/get-transaction-detail.use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [],
-  providers: [CreateTransactionUseCase],
-  exports: [CreateTransactionUseCase],
+  providers: [CreateTransactionUseCase, GetTransactionDetailUseCase],
+  exports: [CreateTransactionUseCase, GetTransactionDetailUseCase],
 })
 export class TransactionsModule {}
