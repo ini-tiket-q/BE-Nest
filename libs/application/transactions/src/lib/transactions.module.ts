f5e17b6f91e0da64fff4ae83502548da26bc5618
IndraNurfa <ahmadindran@gmail.com>
2026-01-26 22:16:15 +0700

    feat: use signature service in usecase

--- libs/application/transactions/src/lib/transactions.module.ts
+++ libs/application/transactions/src/lib/transactions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@tiketq-be/database';
+import { PaymentModule } from '@tiketq-be/payment';
 import { CreateTransactionUseCase } from './use-cases/create-transaction.use-case';
 import { ProcessPaymentCallbackUseCase } from './use-cases/process-payment-callback.use-case';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PaymentModule],
   controllers: [],
   providers: [CreateTransactionUseCase, ProcessPaymentCallbackUseCase],
   exports: [CreateTransactionUseCase, ProcessPaymentCallbackUseCase],
--- libs/application/transactions/src/lib/use-cases/process-payment-callback.use-case.ts
+++ libs/application/transactions/src/lib/use-cases/process-payment-callback.use-case.ts
@@ -1,15 +1,31 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { MidtransSignatureService } from '@tiketq-be/payment';
 
 @Injectable()
 export class ProcessPaymentCallbackUseCase {
   private readonly logger = new Logger(ProcessPaymentCallbackUseCase.name);
-  constructor() {}
+  constructor(
+    private readonly midtransSignatureService: MidtransSignatureService
+  ) {}
 
   async execute(callbackPayload: any): Promise<{ message: string }> {
     this.logger.log(
       `Processing payment callback for order ID: ${callbackPayload.order_id}`
     );
-    // Add your business logic here to process the payment callback
+
+    const isValid = this.midtransSignatureService.verifySignature(
+      callbackPayload.order_id,
+      callbackPayload.status_code,
+      callbackPayload.gross_amount,
+      callbackPayload.signature_key
+    );
+
+    if (!isValid) {
+      this.logger.error(
+        `Failed to verify signature_key order_id ${callbackPayload.order_id}`
+      );
+      throw new NotFoundException('failed to verify signature_key!');
+    }
 
     return { message: 'this is works right!' };
   }
