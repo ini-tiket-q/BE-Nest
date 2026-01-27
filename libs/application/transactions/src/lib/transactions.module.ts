8353de1350620551298db86aa8ae7eaf9889f41c
Lily <lilywinslettt@gmail.com>
2026-01-27 23:10:21 +0800

    feat(transactions): export GetPaymentUrlUseCase from TransactionsModule

--- libs/application/transactions/src/lib/transactions.module.ts
+++ libs/application/transactions/src/lib/transactions.module.ts
@@ -4,11 +4,12 @@ import { PaymentModule } from '@tiketq-be/payment';
 import { CreateTransactionUseCase } from './use-cases/create-transaction.use-case';
 import { ProcessPaymentCallbackUseCase } from './use-cases/process-payment-callback.use-case';
 import { TransactionsInfraModule } from '@tiketq-be/transactions_infra';
+import { GetPaymentUrlUseCase } from '../use-cases/get-payment-url.use-case';
 
 @Module({
   imports: [DatabaseModule, PaymentModule, TransactionsInfraModule],
   controllers: [],
   providers: [CreateTransactionUseCase, ProcessPaymentCallbackUseCase],
-  exports: [CreateTransactionUseCase, ProcessPaymentCallbackUseCase],
+  exports: [CreateTransactionUseCase, ProcessPaymentCallbackUseCase, GetPaymentUrlUseCase],
 })
 export class TransactionsModule {}
