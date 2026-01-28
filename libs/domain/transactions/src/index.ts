52ed940d7aa6e576e3c5bcb1e262786a1ccffe74
Salman <salmanalthof31@gmail.com>
2026-01-28 15:28:45 +0700

    refactor: move file payments.request.port to folder ports in lib

--- libs/domain/transactions/src/index.ts
+++ libs/domain/transactions/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/transactions';
-export * from "./port/payment.request.port"
+export * from './lib/ports/payment.request.port'
 export * from './lib/transaction.entities';
 export * from './lib/ports';
 export * from './lib/dto/create-transaction.dto';
