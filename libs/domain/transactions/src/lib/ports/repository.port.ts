78d3939c3a57f22ecb9d2d74e88ea9a56b6b645a
Salman <salmanalthof31@gmail.com>
2026-01-30 13:18:10 +0700

    feat: paganation in findByEmail

--- libs/domain/transactions/src/lib/ports/repository.port.ts
+++ libs/domain/transactions/src/lib/ports/repository.port.ts
@@ -34,31 +34,43 @@ export interface ITransactionRepositoryPort {
      * @param email - Customer email address
      * @returns Array of transactions for the customer
      */
-    findByCustomerEmail(email: string): Promise<Transaction[]>;
+    findByCustomerEmail(query: QueryByEmail): Promise<Transaction[]>;
 
-    findByUserId(query: TransactionsQuery): Promise<TransactionsDataDto[]>
+    findByUserId(query: QueryByUserId): Promise<TransactionsDataDto[]>
 
     countByUserId(query: CountTransactionsQuery): Promise<number>
 }
 
-export interface TransactionsQuery {
+export interface QueryByUserId extends OptionalQueryUseCase, Paganation {
     userId: string;
+}
+
+export interface QueryByEmail extends OptionalQueryUseCase, Paganation {
+    email: string;
+}
+
+export interface CountTransactionsQuery extends OptionalQueryUseCase {
+    userId?: string;
+    email?: string;
+}
+
+export interface FilterQuery extends OptionalFilter {
+    userId?: string;
+    customerEmail?: string;
+}
+
+interface Paganation {
     page: number;
     limit: number;
+}
+interface OptionalQueryUseCase {
     startDate?: Date;
-    endDate?: Date
-    status?: TransactionStatus
+    endDate?: Date;
+    status?: TransactionStatus;
 }
 
-export interface CountTransactionsQuery {
-    userId: string;
-    startDate?: Date;
-    endDate?: Date
-    status?: TransactionStatus
-}
-
-export interface FilterQueryByUserId {
-    userId: string;
+interface OptionalFilter {
     createdAt?: FindOperator<Date>;
     status?: TransactionStatus
 }
+
