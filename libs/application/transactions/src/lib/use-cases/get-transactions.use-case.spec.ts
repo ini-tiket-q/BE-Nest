5688821ce4216ae1f249536ebc49a420a9a6dd84
Salman <salmanalthof31@gmail.com>
2026-02-03 12:48:45 +0700

    refactor: manage initial testing

--- libs/application/transactions/src/lib/use-cases/get-transactions.use-case.spec.ts
+++ libs/application/transactions/src/lib/use-cases/get-transactions.use-case.spec.ts
@@ -1,9 +1,18 @@
 import { GetTransactionsUseCase } from './get-transactions.use-case';
-import { ITransactionRepositoryPort, TransactionsDataDto } from '@tiketq-be/transactions_domain';
+import { ITransactionRepositoryPort, TransactionsDataDto, TransactionStatus } from '@tiketq-be/transactions_domain';
 
 describe('GetTransactionsUseCase', () => {
   let useCase: GetTransactionsUseCase;
   let transactionRepository: jest.Mocked<ITransactionRepositoryPort>;
+
+  const query = {
+    userId: 'user-1',
+    page: 1,
+    limit: 5,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined,
+  };
 
   beforeEach(() => {
     transactionRepository = {
@@ -18,29 +27,34 @@ describe('GetTransactionsUseCase', () => {
     useCase = new GetTransactionsUseCase(transactionRepository);
   });
 
-  it('should return transactions with correct pagination meta', async () => {
-    // arrange
+  test('return transactions with correct pagination meta', async () => {
     const mockTransactions: TransactionsDataDto[] = [
-      { id: 'trx-1' } as TransactionsDataDto,
-      { id: 'trx-2' } as TransactionsDataDto,
+      { 
+        id: 'trx-1',
+        userId: 'user-1',
+        currency: 'IDR',
+        amount: 1500000,
+        status: TransactionStatus.PAID,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      { 
+        id: 'trx-2',
+        userId: 'user-1',
+        currency: 'IDR',
+        amount: 2500000,
+        status: TransactionStatus.PAID,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
     ];
 
-    transactionRepository.findByUserId.mockResolvedValue(mockTransactions);
-    transactionRepository.countByUserId.mockResolvedValue(12);
+    transactionRepository.findByUserId.mockResolvedValue(mockTransactions)
+    transactionRepository.countByUserId.mockResolvedValue(2);
 
-    const query = {
-      userId: 'user-1',
-      page: 1,
-      limit: 5,
-      startDate: undefined,
-      endDate: undefined,
-      status: undefined,
-    };
-
-    // act
     const result = await useCase.execute(query);
 
-    // assert
+
     expect(transactionRepository.findByUserId).toHaveBeenCalledWith(query);
     expect(transactionRepository.countByUserId).toHaveBeenCalledWith({
       userId: 'user-1',
@@ -54,22 +68,35 @@ describe('GetTransactionsUseCase', () => {
       meta: {
         page: 1,
         limit: 5,
-        total: 12,
-        totalPages: 3,
+        total: 2,
+        totalPages: 1,
       },
     });
   });
 
-  it('should return totalPages = 0 when total is 0', async () => {
+  test('return empty array transactions with totalPages = 0 when total is 0', async () => {
     transactionRepository.findByUserId.mockResolvedValue([]);
     transactionRepository.countByUserId.mockResolvedValue(0);
 
-    const result = await useCase.execute({
+    const result = await useCase.execute(query);
+
+    expect(transactionRepository.findByUserId).toHaveBeenCalledWith(query);
+    expect(transactionRepository.countByUserId).toHaveBeenCalledWith({
       userId: 'user-1',
-      page: 1,
-      limit: 10,
+      startDate: undefined,
+      endDate: undefined,
+      status: undefined,
     });
 
-    expect(result.meta.totalPages).toBe(0);
+    expect(result).toEqual({
+      data: [],
+      meta: {
+        page: 1,
+        limit: 5,
+        total: 0,
+        totalPages: 0,
+      },
+    });
+
   });
 });
