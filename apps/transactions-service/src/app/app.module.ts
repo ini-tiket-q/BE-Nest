ef255a29a24fd7ec083635db284b4e2bde73a5c6
Salman <salmanalthof31@gmail.com>
2026-02-04 22:37:26 +0700

    refactor: delete module payments beacuse not use

--- apps/transactions-service/src/app/app.module.ts
+++ apps/transactions-service/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PaymentsModule } from '../payments/payments.module';
+import { PaymentServiceModule } from './payments/payment-service.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { TransactionsModule } from '@tiketq-be/transactions';
 import { TransactionsServiceModule } from './transactions/transactions-service.module';
@@ -18,7 +18,6 @@ import { DatabaseModule } from '@tiketq-be/database';
     TransactionsModule,
     TransactionsServiceModule,
     PaymentServiceModule,
-    PaymentsModule,
     DatabaseModule
   ],
   controllers: [AppController],
