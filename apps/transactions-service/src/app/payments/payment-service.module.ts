44bb88afbdab022bdc5b708e469953018db3bb17
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2026-02-04 23:42:02 +0700

    refactor: move payments controller

--- apps/transactions-service/src/app/payments/payment-service.module.ts
+++ apps/transactions-service/src/app/payments/payment-service.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionsModule } from '@tiketq-be/transactions';
 import { PaymentWebhookController } from './payment-webhook.controller';
+import { PaymentsController } from './payments.controller';
 
 @Module({
   imports: [TransactionsModule],
-  controllers: [PaymentWebhookController],
+  controllers: [PaymentWebhookController, PaymentsController],
   providers: [],
 })
 export class PaymentServiceModule {}
