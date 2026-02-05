0c2ae41106edd0d5109a44982d317b5a65984235
Aetherx24 <iqbalfahrur@gmail.com>
2026-02-06 04:14:56 +0700

    chore: register booking cancellation dependencies

--- libs/application/flights/src/lib/flights.module.ts
+++ libs/application/flights/src/lib/flights.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BookingController } from 'apps/flight-service/src/bookings/booking.controller';
+import { CancelBookingUseCase } from 'libs/application/flights/src/use-cases/cancel-booking.use-case';
+
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [ BookingController ],
+  providers: [ CancelBookingUseCase ],
+  exports: [ CancelBookingUseCase ],
 })
 export class FlightsModule {}
